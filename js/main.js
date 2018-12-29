9affe928866a0e767032d50ede9501830584e50e
yuvalmund <yuvalmund@gmail.com>
2018-12-29 23:32:32 +0200

    add first events

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ function init_main () {
 
 //bind events to dom elements
 document.addEventListener('DOMContentLoaded', init_main);
+
+$( "#close" ).click(function() {
+    window.close();
+});
+
+var input = document.getElementById("input-text");
+input.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    $("#input-text").val("kaki")
+  }
+});
+
