f6f3170051f5c18baefced1e6ff2de0e68a79575
Yael rabinovitch <yael573@gmail.com>
2019-01-12 13:15:11 +0200

    when click on the extension - open new tab with the images of the pdf pages

---
+++ js/bacgkround.js
@@ -0,0 +1,12 @@
+// on clicked on the extension icon - open new page
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true
+      }, function (tabs) {
+        var tab = tabs[0]
+        var url = tab.url;
+        chrome.tabs.create({ url: chrome.extension.getURL('html/main.html' +  "?tabUrl=" + url ), selected: true });
+      })
+    
+  });
--- js/main.js
+++ js/main.js
@@ -1,64 +1,84 @@
 
+function getTabUrl() {
+  // get tab url from url params
+  var url_string = window.location.href
+  var url = new URL(url_string);
+  var url = url.searchParams.get("tabUrl");
+  return url;
+}
+
 
 function loadCurrentPdf() {
-  // get all current active tabs
-  chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true
-  }, function (tabs) {
-    var tab = tabs[0]
-    var url = tab.url;
 
+var pdfUrl = getTabUrl();
 
-    // The workerSrc property shall be specified.
-    pdfjsLib.GlobalWorkerOptions.workerSrc =
-      './pdf.worker.js';
+  // The workerSrc property shall be specified.
+  pdfjsLib.GlobalWorkerOptions.workerSrc =
+    './pdf.worker.js';
 
-    renderPDF(url);
-  })}
+  renderPDF(pdfUrl);
+}
 
-    function renderPDF(url) {
-      pdfjsLib.getDocument(url).then((pdf) => {
-        for (let i = 1; i <= pdf.numPages; i += 1) {
-          const canvas = document.createElement('canvas');
-          /* This indentify the canvas as the location of the page */
-          canvas.id = i;
-          document.body.appendChild(canvas)
-          pdf.getPage(i).then((page) => {
-            renderPage(page, canvas);
-          })
-        }
+function renderPDF(url) {
+// load pdf from url
+  pdfjsLib.getDocument(url).then((pdf) => {
+
+    // go over every page of the pdf
+    for (let i = 1; i <= pdf.numPages; i += 1) {
+
+      // append title of the page to body
+      var pageTitleDiv = document.createElement('div')
+      pageTitleDiv.innerHTML = "Page " + i;
+      document.body.appendChild(pageTitleDiv)
+      pageTitleDiv.classList.add("page-title");
+
+      // append canvas of the current page to body
+      const canvas = document.createElement('canvas');
+      canvas.id = i;
+      document.body.appendChild(canvas)
+
+      // append hr to body
+      var hr = document.createElement('hr');
+      document.body.appendChild(hr);
+      // render current page
+      pdf.getPage(i).then((page) => {
+        renderPage(page, canvas);
       })
     }
+  })
+}
 
 
 
 
 
-    function renderPage(page, canvas) {
-      const viewport = page.getViewport(1.2);
-      const canvasContext = canvas.getContext('2d');
-      canvasContext.strokeRect(5, 5, 25, 15);
-      canvasContext.scale(2, 2);
-      canvasContext.strokeRect(5, 5, 25, 15);
-      const renderContext = {
-        canvasContext,
-        viewport
-      };
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-      page.render(renderContext).then(rsult => {
-        download(document.getElementById(canvas.id).toDataURL('image/jpeg'), canvas.id  + "page.gif", "image/jpeg");
-        canvasContext.globalAlpha = 0.2;
-        canvasContext.fillStyle = "yellow"; 
-        canvasContext.fillRect(150, 180, 50, 50); 
-        
-      })
-    }
+function renderPage(page, canvas) {
+  const viewport = page.getViewport(1.2);
+  const canvasContext = canvas.getContext('2d');
+  canvasContext.strokeRect(5, 5, 25, 15);
+  canvasContext.scale(2, 2);
+  canvasContext.strokeRect(5, 5, 25, 15);
+  const renderContext = {
+    canvasContext,
+    viewport
+  };
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+  page.render(renderContext).then(rsult => {
+    // download current page
+    download(document.getElementById(canvas.id).toDataURL('image/jpeg'), canvas.id  + "page.gif", "image/jpeg");
+
+    // Draw the marker
+    canvasContext.globalAlpha = 0.2;   // define opacity
+    canvasContext.fillStyle = "yellow"; // define yellow opacity
+    canvasContext.fillRect(150, 180, 50, 50);  // draw the shape - (The x-coordinate , The y-coordinate, The width in px, The height in px)
+
+  })
+}
 
 
 
-  
+
 
 
 
@@ -82,4 +102,4 @@ input.addEventListener("keyup", function (event) {
     $("#input-text").val("kaki")
   }
 })
-  
+
