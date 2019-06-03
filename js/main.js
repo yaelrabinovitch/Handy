76ecbef2789e8f919da77eac7b5f26d680faa474
Yael rabinovitch <yael573@gmail.com>
2019-06-03 19:06:24 +0300

    Add error page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var numOfPages = 1;
 var currResultIndex = 0;
 var results = []
 var textObjects = [];
+var isError = false;
 
 
 //EVENTS
@@ -18,13 +19,14 @@ document.addEventListener('DOMContentLoaded', init_main);
 //INIT
 function init_main() {
   document.getElementById("pdf-container").style.display = "none";
+  document.getElementById("error-container").style.display = "none";
   document.getElementById("previusResultBtn").addEventListener("click", previusResult);
   document.getElementById("nextResultBtn").addEventListener("click", nextResult);
   document.getElementById("input-text").addEventListener("input", onChangeInputText);
+  document.getElementById("reTryLoadBtn").addEventListener("click", reTryLoad);
   $('html').hide().fadeIn('slow');
 
   loadCurrentPdf();
-  pagination();
 }
 
 function getTabUrl() {
@@ -61,7 +63,7 @@ function pagination() {
 }
 
 function ajax(options) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     $.ajax(options).done(resolve).fail(reject);
   });
 }
@@ -125,13 +127,11 @@ function renderPDF(url) {
         renderPage(page, pageCanvas).then(rsult => {
           editCanvas.height = pageCanvas.height;
           editCanvas.width = pageCanvas.width;
-
           var dataUrl = document.getElementById('page_' + pageNum).toDataURL('image/png')
-
           var serverUrl = 'http://193.106.55.32:5000/'
           $.ajax({
             type: 'POST',
-            beforeSend: function(xhr) {
+            beforeSend: function (xhr) {
               activeAjaxConnections++;
             },
             url: serverUrl,
@@ -172,12 +172,13 @@ function renderPDF(url) {
 
               // Add to text objects the current page letters and text
               textObjects.push({ text: text, letters: letters, pageNum: pageNum });
-              
+
             },
             error: function (e) {
+              isError = true;
               activeAjaxConnections--;
               if (0 == activeAjaxConnections) {
-              onFinishGetAllData();
+                onFinishGetAllData();
                 // this was the last Ajax connection, do the thing
               }
               console.log(e);
@@ -189,14 +190,25 @@ function renderPDF(url) {
       })
     }
   })
-  
+
 }
 
-function onFinishGetAllData()
-{
-  document.getElementById("pdf-container").style.display = "block";
-  document.getElementById("loading-container").style.display = "none";
+function onFinishGetAllData() {
+  if (!isError) {
+    document.getElementById("pdf-container").style.display = "block";
+    document.getElementById("loading-container").style.display = "none";
+    pagination();
+  }
+  else {
+    document.getElementById("error-container").style.display = "block";
+    document.getElementById("pdf-container").style.display = "none";
+    document.getElementById("loading-container").style.display = "none";
+  }
+
+
 }
+
+
 
 
 function renderPage(page, canvas) {
@@ -259,9 +271,9 @@ function markerResults() {
     canvasContext1.fillStyle = "yellow";
 
     // marker thr result
-    if(result.length === 1 && result[0].length === 1){
-        canvasContext1.fillRect(result[0][0].location[0], result[0][0].location[1], result[0][0].location[2], result[0][0].location[3]);
-    } else{
+    if (result.length === 1 && result[0].length === 1) {
+      canvasContext1.fillRect(result[0][0].location[0], result[0][0].location[1], result[0][0].location[2], result[0][0].location[3]);
+    } else {
       result.forEach(row => {
         canvasContext1.fillRect(row[1].location[0], row[1].location[1], parseInt(row[0].location[2]) + parseInt(row[0].location[0]) - parseInt(row[1].location[0]), row[0].location[3]);
       });
@@ -294,10 +306,10 @@ function clearResults() {
     canvasContext1.fillStyle = "yellow"; // define yellow opacity
 
 
-      // clean markers
-      if(result.length === 1 && result[0].length === 1){
-        canvasContext1.clearRect(result[0][0].location[0], result[0][0].location[1], result[0][0].location[2], result[0][0].location[3]);
-    } else{
+    // clean markers
+    if (result.length === 1 && result[0].length === 1) {
+      canvasContext1.clearRect(result[0][0].location[0], result[0][0].location[1], result[0][0].location[2], result[0][0].location[3]);
+    } else {
       result.forEach(row => {
         canvasContext1.clearRect(row[1].location[0], row[1].location[1], parseInt(row[0].location[2]) + parseInt(row[0].location[0]) - parseInt(row[1].location[0]), row[0].location[3]);
       });
@@ -347,7 +359,7 @@ function onChangeInputText() {
 
       textObj.letters.forEach(letter => {
         if (indices.includes(letter.index)) {
-            results.push(arrangeResultToRows(letter.index, inputText, textObj));
+          results.push(arrangeResultToRows(letter.index, inputText, textObj));
         }
       });
     });
@@ -360,17 +372,17 @@ function onChangeInputText() {
   scrollToCurrentResults(currResultIndex);
 }
 
-function arrangeResultToRows(index, inputText, textObj){
-  if(inputText.length === 1) {
+function arrangeResultToRows(index, inputText, textObj) {
+  if (inputText.length === 1) {
     return [[textObj.letters[index]]];
-  } else{
+  } else {
     let arr = [];
     let charsArr = [...inputText];
     let rows = {}
 
-    for(let i = index; i < charsArr.length + index; i++) {
+    for (let i = index; i < charsArr.length + index; i++) {
       const y = parseInt(textObj.letters[i].location[1]);
-      if(y > 0){
+      if (y > 0) {
         if (!rows[y]) rows[y] = []
         rows[y].push(i);
       }
@@ -378,7 +390,7 @@ function arrangeResultToRows(index, inputText, textObj){
 
     Object.keys(rows).forEach(y => {
       let first = rows[y][0],
-          last = rows[y][rows[y].length - 1];
+        last = rows[y][rows[y].length - 1];
       arr.push([textObj.letters[first], textObj.letters[last]]);
     });
     return arr;
@@ -401,6 +413,10 @@ function getIndicesOf(searchStr, str, caseSensitive) {
     startIndex = index + searchStrLen;
   }
   return indices;
+}
+
+function reTryLoad() {
+  window.location.reload()
 }
 
 
